aa58fcaeab962cf296bf73dadc85fcdd37117c0e
jonathanmeier5 <jonathanwmeier@gmail.com>
2015-11-27 01:12:16 -0500

    fixed base url issue

--- API/urls.py
+++ API/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.base, name='base'),
+    url(r'^$', views.base, name='base'),
     url(r'^friends/(?P<friend_count>(.*))/$', views.get_friends,
         name='add_friends'),
     url(r'^friends/add/$', views.add_friends,name='add_friends'),
